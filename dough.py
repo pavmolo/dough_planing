6e6da4b4a94132611a95a46bf47bbd662b39163f
pavmolo <74321783+pavmolo@users.noreply.github.com>
2023-11-17 07:51:28 +0300

    Update dough.py

--- dough.py
+++ dough.py
@@ -5,6 +5,6 @@ from io import BytesIO
 
 st.markdown('''<h3>Файл с данными</h3>''', unsafe_allow_html=True)
 df = st.file_uploader("Выберите XLSX файл с данными", accept_multiple_files=False)
-
-df = pd.read_excel(df)
-st.dataframe(df)
+if df: 
+  df = pd.read_excel(df)
+  st.dataframe(df)
