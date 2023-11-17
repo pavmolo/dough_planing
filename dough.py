f0385e726673c7675be8d1bbdf30314517a23400
pavmolo <74321783+pavmolo@users.noreply.github.com>
2023-11-17 11:57:55 +0300

    Update dough.py

--- dough.py
+++ dough.py
@@ -103,10 +103,21 @@ def schedule_oven_operations(start_shift, end_shift, num_ovens, change_trolley_t
         if end_baking_time > end_shift:
             continue  # Если время выпекания выходит за пределы смены, пропустим эту вагонетку
         
+        # Расчет длительности выпекания
+        duration = (end_baking_time - start_baking_time).seconds // 60  # в минутах
+        
+        # Состав вагонетки
+        trolley_composition = ', '.join([
+            f"{product}: {int(trolley[product])} листов ({int(trolley[product]) * df.loc[df['Наименование товара'] == product, 'Количество на листе'].iloc[0]} штук)"
+            for product in df['Наименование товара'].unique() if trolley.get(product, 0) > 0
+        ])
+        
         # Добавление операции выпекания в расписание печи
         ovens_schedule[next_oven].append({
             'Начало': start_baking_time.time(),
             'Конец': end_baking_time.time(),
+            'Длительность': duration,
+            'Состав вагонетки': trolley_composition,
             'Вагонетка': trolley.name,
             'Температура Печи': trolley['Температура Печи']
         })
@@ -114,50 +125,7 @@ def schedule_oven_operations(start_shift, end_shift, num_ovens, change_trolley_t
         # Обновление времени последней операции в печи
         last_operation_time[next_oven] = end_baking_time
 
-    return ovens_schedule
 
-# Теперь переопределим функцию create_full_baking_schedule с учетом наличия df_products_sorted
-def create_full_baking_schedule(df, temp_for_oven1, start_time, reset_time=10, basket_interval=3):
-    # Инициализация начального времени для каждой печи
-    oven1_time = pd.to_datetime(start_time)
-    oven2_time = pd.to_datetime(start_time)
-    # Последняя температура для определения необходимости перенастройки
-    last_temp_oven1 = temp_for_oven1
-    last_temp_oven2 = None
-
-    # Инициализация расписания выпекания
-    baking_schedule = []
-
-    # Перебор SKU для планирования по печам
-    for index, row in df.iterrows():
-        oven_time = oven1_time if row['Температура Печи'] == temp_for_oven1 else oven2_time
-        oven_name = "Печь 1" if row['Температура Печи'] == temp_for_oven1 else "Печь 2"
-        last_temp = last_temp_oven1 if oven_name == "Печь 1" else last_temp_oven2
-
-        # Если температура изменилась, добавляем время на перенастройку
-        if last_temp != row['Температура Печи']:
-            baking_schedule.append([oven_name, "Перенастройка", "", last_temp, oven_time, "", "", reset_time])
-            oven_time += timedelta(minutes=reset_time)
-
-        # Планирование выпекания для каждой вагонетки
-        for basket in range(1, row['Кол-во вагонеток'] + 1):
-            baking_schedule.append([oven_name, row['Наименование товара'], basket, row['Температура Печи'], oven_time, oven_time - timedelta(hours=1.4), oven_time - timedelta(hours=3.3), row['Время']])
-            oven_time += timedelta(minutes=row['Время'] + basket_interval)
-
-        # Обновляем время последней выпекаемой вагонетки и температуру для печи
-        if oven_name == "Печь 1":
-            oven1_time = oven_time
-            last_temp_oven1 = row['Температура Печи']
-        else:
-            oven2_time = oven_time
-            last_temp_oven2 = row['Температура Печи']
-        df = pd.DataFrame(baking_schedule, columns=["Печь", "Наименование товара", "Вагонетка", "Температура Печи", "Время начала выпекания", "Время формовки", "Время замеса", "Длительность"])
-        # Преобразование строк в datetime объекты, а затем форматирование их как 'HH:MM'
-        df['Время начала выпекания'] = pd.to_datetime(df['Время начала выпекания']).dt.strftime('%H:%M')
-        df['Время формовки'] = pd.to_datetime(df['Время формовки']).dt.strftime('%H:%M')
-        df['Время замеса'] = pd.to_datetime(df['Время замеса']).dt.strftime('%H:%M')
-
-    return df
 def to_df_from_list(dicti):
     schedule_df = pd.DataFrame(dicti)
     # Убедитесь, что 'Начало' и 'Конец' являются правильными временными метками, прежде чем преобразовывать их
