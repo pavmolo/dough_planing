20b3c00caa9416a44619b4fcd7948ddb5aae810d
pavmolo <74321783+pavmolo@users.noreply.github.com>
2023-11-17 07:49:29 +0300

    Update dough.py

--- dough.py
+++ dough.py
@@ -4,4 +4,7 @@ import pandas as pd
 from io import BytesIO
 
 st.markdown('''<h3>Файл с данными</h3>''', unsafe_allow_html=True)
-master_data_file = st.file_uploader("Выберите XLSX файл с мастер данными", accept_multiple_files=False)
+df = st.file_uploader("Выберите XLSX файл с данными", accept_multiple_files=False)
+
+df = pd.read_excel(df)
+st.dataframe(df)
