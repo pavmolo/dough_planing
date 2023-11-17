4182972fb98e0cff0350b32f52f41fb0ec429e3d
pavmolo <74321783+pavmolo@users.noreply.github.com>
2023-11-17 08:00:31 +0300

    Update dough.py

--- dough.py
+++ dough.py
@@ -2,6 +2,7 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 from io import BytesIO
+from datetime import timedelta
 
 
 # Теперь переопределим функцию create_full_baking_schedule с учетом наличия df_products_sorted
