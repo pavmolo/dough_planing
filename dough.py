1256a787ed348e3b520746f119cb8e05625f1b48
pavmolo <74321783+pavmolo@users.noreply.github.com>
2023-12-15 15:44:45 +0300

    Update dough.py

--- dough.py
+++ dough.py
@@ -271,7 +271,7 @@ if uploaded_file:
         axis=1
     ) 
     zuvalashka_df = pd.pivot_table(zuvalashka_start, values='ШТ', index=['Время начала изг. зуваляшек', 'Время оконч. изг. зуваляшек', 'Размер зуваляшки, гр', 'Тип теста'], aggfunc='sum')
-    dough_zero = zuvalashka_df
+    dough_zero = zuvalashka_df.copy()
     dough_zero = dough_zero.reset_index()
     dough_master = pd.pivot_table(zuvalashka_start, values=['Приготовление опары, мин', 'Замес теста, мин', 'Первая отстойка, мин', 'Вторая отскойка, мин.'], index='Тип теста', aggfunc='mean')
     dough_master = dough_master.reset_index()
