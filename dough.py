878b5913c42e74d82618335fe80d16bbb2749c85
pavmolo <74321783+pavmolo@users.noreply.github.com>
2023-12-16 11:10:32 +0300

    Update dough.py

--- dough.py
+++ dough.py
@@ -272,10 +272,10 @@ if uploaded_file:
         axis=1
     )
     zuvalashka_start
-    zuvalashka_df = pd.pivot_table(zuvalashka_start, 
-                               values='ШТ', 
-                               index=['Время начала изг. зуваляшек', 'Время оконч. изг. зуваляшек', 'Тип теста', 'Размер зуваляшки, гр'], 
-                               aggfunc='sum')
+    zuvalashka_df = zuvalashka_start.groupby(['Время начала изг. зуваляшек', 
+                                          'Время оконч. изг. зуваляшек', 
+                                          'Тип теста', 
+                                          'Размер зуваляшки, гр']).agg({'ШТ': 'sum'}).reset_index()
     zuvalashka_df
     dough_zero = zuvalashka_df.reset_index()
     dough_zero
